a943ea0389be0aa8a7430d635ced3d22d517eb4c
Rohan Nagar <rohan.nagar@utexas.edu>
2016-06-16 20:00:53 -0700

    Working with real messages

--- site.js
+++ site.js
@@ -2,32 +2,52 @@ $submit_button = $('#submit')
 $result_table = $('#table')
 $textfield = $('#search-box')
 
-// Submit button onclick
-$(function () {
-        $submit_button.click(function () {
-            readJSON();
-        });
-    });
+allMessages = null
 
-function readJSON() {
-	$.getJSON("test.json", function(json) {
-		displayMessages(json);
-	});
+function setMessages(messages) {
+    allMessages = messages
 }
 
-function displayMessages(messages) {
-	console.log(messages);
-	console.log($textfield.val());
-	$result_table.bootstrapTable({
-    		columns: [{
-        		field: 'date',
-        		title: 'Date'
-    		}, {
-        		field: 'name',
-        		title: 'Sender'
-    		}, {
-        		field: 'text',
-        		title: 'Message'
-    		}],
-    		data: messages});
+// Load JSON from file
+$.getJSON("messages.json", function(json) {	
+    setMessages(json);
+});
+
+// Submit button onclick
+$(function () {
+    $submit_button.click(function () {
+        displayMessages();
+    });
+});
+
+function messageContains(message, phrase) {
+    return message.indexOf(phrase) > -1;
+}
+
+function displayMessages() {
+    console.log($textfield.val());
+
+    var displayMessages = []
+    for (var i = 0; i < allMessages.length; i++) {
+        if (messageContains(allMessages[i].text, $textfield.val())) {
+            displayMessages.push(allMessages[i]);
+        }
+    }
+    console.log(displayMessages);
+
+    $result_table.bootstrapTable({
+    	columns: [{
+            field: 'date',
+            title: 'Date'
+    	}, {
+            field: 'name',
+            title: 'Sender'
+    	}, {
+            field: 'text',
+            title: 'Message'
+    	}],
+    	data: displayMessages
+    });
+
+    $result_table.bootstrapTable("load", displayMessages)
 }
